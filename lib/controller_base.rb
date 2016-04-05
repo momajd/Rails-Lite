1d4ffe217c2a825e431b738ca5068932c85b4921
momajd <mo.a.majd@gmail.com>
2016-04-05 15:29:10 -0700

    start project

---
+++ lib/controller_base.rb
@@ -0,0 +1,48 @@
+require 'active_support'
+require 'active_support/core_ext'
+require 'erb'
+require_relative './session'
+
+class ControllerBase #equivalent to Rails' ActionController::Base - MAM
+  attr_reader :req, :res, :params
+
+  # Setup the controller
+  def initialize(req, res)
+    @req = req
+    @res = res
+  end
+
+  # Helper method to alias @already_built_response
+  def already_built_response?
+    @already_built_response ||= false
+  end
+
+  # Set the response status code and header
+  def redirect_to(url)
+  end
+
+  # Populate the response with content.
+  # Set the response's content type to the given type.
+  # Raise an error if the developer tries to double render.
+  def render_content(content, content_type)
+    raise StandardError if already_built_response?
+
+    @res['Content-Type'] = content_type
+    @res.body = [content] #TODO: why does this need to be an array? - MAM
+
+    @already_built_response = true
+  end
+
+  # use ERB and binding to evaluate templates
+  # pass the rendered html to render_content
+  def render(template_name)
+  end
+
+  # method exposing a `Session` object
+  def session
+  end
+
+  # use this with the router to call action_name (:index, :show, :create...)
+  def invoke_action(name)
+  end
+end
